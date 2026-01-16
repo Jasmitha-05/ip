7bc77c9b08b84cf850132f947b559c81c6561050
Jasmitha <E1354461@u.nus.edu>
2026-01-16 20:42:26 +0800

    Initial skeletal version
    Remained the main class to new ChatBot Name and added greeting and exit.

---
+++ src/main/java/Stitch.java
@@ -0,0 +1,13 @@
+public class Stitch {
+    public static void main(String[] args) {
+        String ChatBotName = "Stitch";
+
+        System.out.println("_________________________________");
+        System.out.println("Hello! I'm " + ChatBotName);
+        System.out.println("What can I do for you?");
+        System.out.println("_________________________________");
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_________________________________");
+    }
+}
