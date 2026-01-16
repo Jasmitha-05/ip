6e60e719017596e724b9990051fb1e21f2c2b554
Jasmitha <E1354461@u.nus.edu>
2026-01-16 21:45:33 +0800

    Add, List
    store texts entered by user and display them when prompted by the command "list"

--- src/main/java/Stitch.java
+++ src/main/java/Stitch.java
@@ -4,6 +4,8 @@ public class Stitch {
     public static void main(String[] args) {
         String chatBotName = "Stitch";
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int numOfTasks = 0;
 
         System.out.println("     _________________________________");
         System.out.println("     Hello! I'm " + chatBotName);
@@ -19,13 +21,30 @@ public class Stitch {
                 System.out.println("     _________________________________");
                 break;
 
-            } else {
+            } else if (userInput.equalsIgnoreCase("list")) {
                 System.out.println("     _________________________________");
-                System.out.println("     " + userInput);
+                if (numOfTasks == 0) {
+                    System.out.println("     Good job! You have no more tasks.");
+                } else {
+                    int order = 1;
+                    for (int i = 0; i < numOfTasks; i++) {
+                        System.out.println("     " + order + ". " + tasks[i]);
+                        order++;
+                    }
+                }
                 System.out.println("     _________________________________");
             }
-        }
 
-        scanner.close();
+            else {
+                    tasks[numOfTasks] = userInput;
+                    numOfTasks++;
+                    System.out.println("     _________________________________");
+                    System.out.println("     added: " + userInput);
+                    System.out.println("     _________________________________");
+                }
+            }
+
+             scanner.close();
+        }
+ 
     }
-}
