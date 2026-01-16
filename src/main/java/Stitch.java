bc908bc60a8f0ebc1224402e7ec1bc7221aff759
Jasmitha <E1354461@u.nus.edu>
2026-01-16 21:13:44 +0800

    Echo
    repeats all command entered by user and exits when user enters bye

--- src/main/java/Stitch.java
+++ src/main/java/Stitch.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
+
 public class Stitch {
     public static void main(String[] args) {
-        String ChatBotName = "Stitch";
+        String chatBotName = "Stitch";
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("_________________________________");
-        System.out.println("Hello! I'm " + ChatBotName);
-        System.out.println("What can I do for you?");
-        System.out.println("_________________________________");
+        System.out.println("     _________________________________");
+        System.out.println("     Hello! I'm " + chatBotName);
+        System.out.println("     What can I do for you?");
+        System.out.println("     _________________________________");
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_________________________________");
+        while(true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("     _________________________________");
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("     _________________________________");
+                break;
+
+            } else {
+                System.out.println("     _________________________________");
+                System.out.println("     " + userInput);
+                System.out.println("     _________________________________");
+            }
+        }
+
+        scanner.close();
     }
 }
