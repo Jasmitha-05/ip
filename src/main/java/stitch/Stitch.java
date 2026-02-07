1a5dcb2a8ccd7d0bd50a1fc04056dba93d83937b
Jasmitha <E1354461@u.nus.edu>
2026-02-07 17:51:07 +0800

    Add assertions
    Use assert to catch any programmer error so that it can be fixed to prevent potential bugs

--- src/main/java/stitch/Stitch.java
+++ src/main/java/stitch/Stitch.java
@@ -31,25 +31,35 @@ public class Stitch {
     public String getResponse(String userInput) {
         try {
             String[] parsedInput = Parser.parse(userInput);
+            assert parsedInput != null : "Parser returning null";
+            assert parsedInput.length > 0 : "no command parsed";
 
             switch (parsedInput[0]) {
             case "list":
                 return taskList.displayAllTasks();
             case "mark":
+                assert parsedInput.length == 2 : "wrong format for mark";
                 return taskList.markTask(Integer.parseInt(parsedInput[1]));
             case "unmark":
+                assert parsedInput.length == 2 : "wrong format for unmark";
                 return taskList.unmarkTask(Integer.parseInt(parsedInput[1]));
             case "todo":
+                assert parsedInput.length == 2 : "wrong format for todo";
                 return taskList.todoTask(parsedInput[1]);
             case "deadline":
+                assert parsedInput.length == 3 : "wrong format for deadline";
                 return taskList.deadlineTask(parsedInput[1], parsedInput[2]);
             case "event":
+                assert parsedInput.length == 4 : "wrong format for event";
                 return taskList.eventTask(parsedInput[1], parsedInput[2], parsedInput[3]);
             case "delete":
+                assert parsedInput.length == 3 : "wrong format for delete";
                 return taskList.deleteTask(Integer.parseInt(parsedInput[1]));
             case "search":
+                assert parsedInput.length == 2 : "wrong format for search";
                 return taskList.sameDateTask(parsedInput[1]);
             case "find":
+                assert parsedInput.length == 2 : "wrong format for find";
                 return taskList.findTask(parsedInput[1]);
             case "bye":
                 return ui.showBye();
