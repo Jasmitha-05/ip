965d253eca894bbdd149ab200d04e5e9eb3f0d8e
Jasmitha <E1354461@u.nus.edu>
2026-02-18 13:08:27 +0800

    Update import ordering

--- src/main/java/stitch/MainWindow.java
+++ src/main/java/stitch/MainWindow.java
@@ -1,5 +1,7 @@
 package stitch;
 
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -7,8 +9,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.animation.PauseTransition;
-import javafx.application.Platform;
 import javafx.util.Duration;
 
 /**
